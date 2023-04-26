ded5f9607b7303958c3ecdd57618d8b8fea02efa
samuelmarinsoto <samarin@estudiantec.cr>
2023-04-26 00:33:45 -0600

    logica colisiones, listas enemigos y balas amigas, logica para remover items no presentes en el canvas de dichas listas, eliminar condiciones de carrera al ejecutar funcion de colisiones solo una vez cada cierto tiempo dentro de funcion de mover a los enemigos. gracias a jose por esa

--- juego.py
+++ juego.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import time
+import random
 from PIL import Image, ImageTk
 from threading import Thread
 import pygame
@@ -27,6 +28,24 @@ import pygame
 # [2] otro problema es una condicion de carrera entre las balas amikas y enemikas. si una itera y encuentra que toco
 # a la otra, se va a desaparecer y mandar pa fuera antes de que la otra haga lo mismo, entonces la otra nunca desaparece
 
+# TODO: pereguntar a jose sobre try except
+amikos = []
+enemikos = []
+bidas = 3
+dificultad = 0.01
+
+def encontrar_i(lista, num, i):
+    if lista[i] == num:
+        return i
+    else:
+        return encontrar_i(lista, num, i+1)
+
+def mas_dificil():
+    global dificultad
+    while dificultad:
+        dificultad += 0.01
+        time.sleep(0.3)
+        
 def inicio():
     venjuego = tk.Tk()
     venjuego.configure(bg="#FFFAE4")
@@ -42,7 +61,41 @@ def inicio():
     phberde = ImageTk.PhotoImage(Image.open("bichoberde.png"))
 
     jugador = lienzo.create_image(0, 500, image=phjugador)
-	
+
+    def reciclaje(objeto, i, paso):
+        if paso == 0:
+            global enemikos
+            if i == len(enemikos):
+                return reciclaje(objeto, 0, 1)
+            elif enemikos[i] == objeto:
+                enemikos.pop(i)
+                return 0
+            else:
+                return reciclaje(objeto, i+1, 0)
+        else:
+            global amikos
+            if i == len(amikos):
+                return 0
+            elif amikos[i] == objeto:
+                amikos.pop(i)
+                return 0
+            else:
+                return reciclaje(objeto, i+1, 1)
+            
+
+    def colisiones(lista, objeto):
+        if lista == []:
+            return False
+        ocoord = lienzo.bbox(objeto)
+        lcoord = lienzo.bbox(lista[0])
+        if (lcoord != None) and (ocoord != None) and (lcoord[0] > ocoord[0]) and (lcoord[2] < ocoord[2]) and (lcoord[1] > ocoord[1]) and (lcoord[3] < ocoord[3]):
+            lienzo.delete(objeto)
+            lienzo.delete(lista[0])
+            reciclaje(objeto, 0, 0)
+            reciclaje(lista[0], 0, 0)
+        else:
+            return colisiones(lista[1:], objeto)
+                
     def movimiento():
 
         def arriba(event=None):
@@ -60,40 +113,77 @@ def inicio():
         def disparo(event=None):
         	jugador_coord = lienzo.bbox(jugador)
         	bala = lienzo.create_image(jugador_coord[2], jugador_coord[3]-50, image=phbala)
+
+        	global amikos
+        	amikos += [bala]
         	
         	def bala_mov():
-        		bala_coord = lienzo.bbox(bala)
-        		while bala_coord[2] < lienzo.winfo_width():
-        			bala_coord = lienzo.bbox(bala)
-        			print(bala_coord)
-        			time.sleep(0.01)
+        		coord = lienzo.bbox(bala)
+        		while (coord != None) and (coord[2] <= lienzo.winfo_width()):
         			lienzo.move(bala, 6, 0)
-        		print(bala)
-        		lienzo.delete(bala)
-	
+        			time.sleep(0.01)
+        			coord = lienzo.bbox(bala)
+        		if (coord != None) and coord[2] > lienzo.winfo_width():
+        		    lienzo.delete(bala)
+        		    reciclaje(bala, 0, 0)
+        		
         	bala_hilo = Thread(target=bala_mov)
         	bala_hilo.daemon = True
         	bala_hilo.start()
+
 			
         venjuego.bind('w', arriba)
         venjuego.bind('a', izquierda)
         venjuego.bind('s', abajo)
         venjuego.bind('d', derecha)
-        venjuego.bind('k', disparo)
+        venjuego.bind('<Button-1>', disparo)
 
     def berde():
     	bicho = lienzo.create_image(random.randint(1400, 1550), random.randint(50, 950), image=phberde)
 
-    	def berde_mov():
-    		berde_coord = lienzo.bbox(bicho)
+    	global enemikos
+    	enemikos += [bicho]
 
-    def bichos():
-    	berde()
+    	xveloz = random.randint(-4,0)
+    	yveloz = random.randint(-4,4)
+		
+    	def berde_mov(xveloz, yveloz):
+    	    global amikos
+    	    coord = lienzo.bbox(bicho)
+    	    while (coord != None) and not (colisiones(amikos, bicho)) and coord[2] > 0:
+    		    if (coord[3] >= (lienzo.winfo_height()) or coord[1] < 0):
+    		        yveloz = -yveloz
+    		    lienzo.move(bicho, xveloz, yveloz)
+    		    time.sleep(0.01)
+    		    coord = lienzo.bbox(bicho)
+    	    if (coord != None) and coord[2] < 0:
+    	        lienzo.delete(bicho)
+    	        reciclaje(bicho, 0, 0)
+
+    	mov_hilo = Thread(target=berde_mov, args=[xveloz, yveloz])
+    	mov_hilo.daemon = True
+    	mov_hilo.start()
+
+    def gen_berde():
+        global dificultad
+        while dificultad:
+            print(dificultad)
+            berde()
+            time.sleep(dificultad)
+
 			
     def hilos():
-        movimiento_hilo = Thread(target=movimiento)
-        movimiento_hilo.daemon = True
-        movimiento_hilo.start()
+        dif_hilo = Thread(target=mas_dificil)
+        dif_hilo.daemon = True
+        dif_hilo.start()
+        
+        mov_hilo = Thread(target=movimiento)
+        mov_hilo.daemon = True
+        mov_hilo.start()
 
+        berde_hilo = Thread(target=gen_berde)
+        berde_hilo.daemon = True
+        berde_hilo.start()
+        
     hilos()
     venjuego.mainloop()
